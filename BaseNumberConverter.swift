ae8df96f3ca64121064f7b5b2763cfefcee6c777
IcarusTyler <matthias.zarzecki@gmail.com>
2019-03-05 10:55:09 +0100

    Update BaseNumberConverter.swift

--- BaseNumberConverter.swift
+++ BaseNumberConverter.swift
@@ -131,14 +131,14 @@ class BaseConverter {
     
     /**
      Returns an array with the correct numerals that are used to display a number in the specified base.
-     Bases up to 85 share the same numerals, up to the point where the base terminates. Base58 and Base64 have unique numerals.
+     Bases 2 to 85 share the same numerals, up to the point where the base terminates. Base58 and Base64 have unique numerals.
      Bases 86-160 use a different set of numerals.
-     - parameter forBase: The base that the numerals should be able to display. Must be > 1 and <= 160
+     - parameter forBase: The base that the numerals should be able to display. Must be >= 2 and <= 160
      - returns: An array of single characters as strings that are used to assembled a number in the given base
      */
     static private func getNumerals(forBase base: Int = 10) -> [String] {
         switch base {
-        case 1...57, 59...63, 65...85:
+        case 2...57, 59...63, 65...85:
             //Get the numerals from base85 up to the needed amount
             return Array(base85Numerals.prefix(base))
         case 58:
@@ -168,3 +168,55 @@ class BaseConverter {
         return letterArray
     }
 }
+
+
+BaseConverter.convert("0", fromBase: 2, toBase: 10)       //0
+BaseConverter.convert("1", fromBase: 2, toBase: 10)       //1
+BaseConverter.convert("10", fromBase: 2, toBase: 10)      //2
+BaseConverter.convert("11", fromBase: 2, toBase: 10)      //3
+BaseConverter.convert("100", fromBase: 2, toBase: 10)     //4
+
+BaseConverter.convert("0", fromBase: 10, toBase: 2)       //0
+BaseConverter.convert("1", fromBase: 10, toBase: 2)       //1
+BaseConverter.convert("2", fromBase: 10, toBase: 2)       //10
+BaseConverter.convert("11", fromBase: 10, toBase: 2)      //1011
+BaseConverter.convert("12", fromBase: 10, toBase: 2)      //1100
+BaseConverter.convert("13", fromBase: 10, toBase: 2)      //1101
+BaseConverter.convert("-13", fromBase: 10, toBase: 2)     //-1101
+
+BaseConverter.convert("0", fromBase: 10, toBase: 3)       //0
+BaseConverter.convert("1", fromBase: 10, toBase: 3)       //1
+BaseConverter.convert("2", fromBase: 10, toBase: 3)       //2
+BaseConverter.convert("3", fromBase: 10, toBase: 3)       //10
+BaseConverter.convert("-13", fromBase: 10, toBase: 3)     //-111
+
+BaseConverter.convert("0", fromBase: 10, toBase: 36)      //0
+BaseConverter.convert("1", fromBase: 10, toBase: 36)      //1
+BaseConverter.convert("2", fromBase: 10, toBase: 36)      //2
+BaseConverter.convert("3", fromBase: 10, toBase: 36)      //3
+BaseConverter.convert("17", fromBase: 10, toBase: 36)     //H
+BaseConverter.convert("22", fromBase: 10, toBase: 36)     //M
+BaseConverter.convert("35", fromBase: 10, toBase: 36)     //Z
+BaseConverter.convert("36", fromBase: 10, toBase: 36)     //10
+BaseConverter.convert("37", fromBase: 10, toBase: 36)     //11
+BaseConverter.convert("-37", fromBase: 10, toBase: 36)    //-11
+
+BaseConverter.convert("-37", fromBase: 8, toBase: 4)      //-133
+BaseConverter.convert("84", fromBase: 10, toBase: 85)     //~
+
+BaseConverter.convert("61", fromBase: 10, toBase: 62)     //z
+BaseConverter.convert("62", fromBase: 10, toBase: 62)     //10
+BaseConverter.convert("2", fromBase: 10, toBase: 2)       //10
+BaseConverter.convert("2", fromBase: 10, toBase: 1)       //||
+BaseConverter.convert("4", fromBase: 10, toBase: 1)       //||||
+
+BaseConverter.convert("||||", fromBase: 1, toBase: 10)    //4
+BaseConverter.convert("1263", fromBase: 10, toBase: 85)   //E<
+BaseConverter.convert("84", fromBase: 10, toBase: 85)     //~
+
+BaseConverter.convert("159", fromBase: 10, toBase: 160)   //ラ
+BaseConverter.convert("160", fromBase: 10, toBase: 160)   //10
+
+BaseConverter.convert("1263", fromBase: 10, toBase: 161)  //nil (outside base range)
+BaseConverter.convert("1263", fromBase: 0, toBase: 86)    //nil (outside base range)
+BaseConverter.convert("", fromBase: 3, toBase: 86)        //nil (faulty input number)
